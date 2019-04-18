cc46f61da36cd0ff049a8ef7853751da6cd66cba
Bhavisha <bksahitya@gmail.com>
2019-04-18 22:38:48 +0530

    updated login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormComponent } from './form/form.component';
 import {FormsModule} from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
+import { ToastrModule } from 'ngx-toastr';
 import {RouterModule,Routes} from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { ServerService } from './server.service';
 
 const appRoute:Routes =[
   {
@@ -29,12 +32,19 @@ const appRoute:Routes =[
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoute)
+    BrowserAnimationsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoute),
+    ToastrModule.forRoot({ 
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    })
   ],
   exports: [
     RouterModule
   ],
-  providers: [],
+  providers: [ServerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import {RouterModule,Routes} from '@angular/router';
 import {Router} from '@angular/router';
+import {Login} from '../login';
+import {FormGroup, Form} from '@angular/forms';
+import { ServerService } from '../server.service';
+import {ToastrService} from 'ngx-toastr';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  
 })
 export class LoginComponent implements OnInit {
-
+  loginUser: Login={} as any;
+    form:FormGroup;
   public show: boolean = false;
 public buttonName: any = true;
 
-toggle() {
-    this.show = !this.show;
+addUser(form: any) {
+  console.log(form.value);
+ // this.toastr.success('Success', "hey");
+  this.loginUser={username:form.value.name, password:form.value.password}
+  this.serverService.loginUser(this.loginUser)
+  .subscribe(
+    (response) =>{ if("login success" === response)
+                      {
+                      sessionStorage.setItem(name,this.loginUser.username);
+                      this.router.navigate(['/form'])
+                      this.toastr.success('Success', "Logged In Successfully");
+                      }else
+                      {
+                        
+                        this.toastr.error('Failed', "Invalid Credentials");
+                      }
+                      console.log(response);  
+                  },
+    (error) =>console.log(error)
+  );
+
     
 }
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private serverService:ServerService,private toastr: ToastrService) { }
   onSubmit()
   {
-    this.router.navigateByUrl('/form');
+    //this.router.navigateByUrl('/form');
+    this.toastr.success('Success', "hey");
   }
   ngOnInit() {
   }
